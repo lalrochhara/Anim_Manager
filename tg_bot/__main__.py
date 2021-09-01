a8ce963e78cad22f2c7e5894d1937b4b3a188cdd
Senu Gamer Boy <86920820+SenuGamerBoy@users.noreply.github.com>
2021-09-01 13:55:16 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -48,12 +48,12 @@ This is the help section where you can learn to use me. "yay!🥳"
 👇👇👇👇👇👇👇👇👇👇👇👇
 
 Main commands available:
- - /start: start the bot
- - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
- - /settings:
-   - in PM: will send you your settings for all supported modules.
-   - in a group: will redirect you to pm, with all that chat's settings.
+ 💠 - /start: start the bot
+ 💠 - /help: PM's you this message.
+ 💠 - /help <module name>: PM's you info about that module.
+ 💠 - /settings:
+    🔹- in PM: will send you your settings for all supported modules.
+    🔹- in a group: will redirect you to pm, with all that chat's settings.
 
 
 All commands can either be used with / or !.
